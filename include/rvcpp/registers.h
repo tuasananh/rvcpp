cd2cac8e72f654a4c65c9c7ac3c862de34ca4d33
kilkuwu <iamkilkuwu@gmail.com>
2025-11-22 18:12:53 +0700

    refactor(registers): :fire: remove redundant register accessors and simplify constructor

--- include/rvcpp/registers.h
+++ include/rvcpp/registers.h
@@ -15,39 +15,5 @@ namespace rvcpp {
 
     Register read(size_t index) const;
     void write(size_t index, Register value);
-
-    Register& zero();
-    Register& ra();
-    Register& sp();
-    Register& gp();
-    Register& tp();
-    Register& t0();
-    Register& t1();
-    Register& t2();
-    Register& s0();
-    Register& fp();
-    Register& s1();
-    Register& a0();
-    Register& a1();
-    Register& a2();
-    Register& a3();
-    Register& a4();
-    Register& a5();
-    Register& a6();
-    Register& a7();
-    Register& s2();
-    Register& s3();
-    Register& s4();
-    Register& s5();
-    Register& s6();
-    Register& s7();
-    Register& s8();
-    Register& s9();
-    Register& s10();
-    Register& s11();
-    Register& t3();
-    Register& t4();
-    Register& t5();
-    Register& t6();
   };
 }  // namespace rvcpp
--- source/registers.cpp
+++ source/registers.cpp
@@ -2,8 +2,10 @@
 
 namespace rvcpp {
   Registers::Registers(Register _pc, Register sp, Register gp) : x{}, pc(_pc) {
-    this->sp() = sp;
-    this->gp() = gp;  // SP
+    constexpr size_t SP_REGISTER = 2;
+    constexpr size_t GP_REGISTER = 3;
+    this->x[SP_REGISTER] = sp;  // SP
+    this->x[GP_REGISTER] = gp;  // GP
   }
 
   Register Registers::read(size_t index) const {
@@ -19,41 +21,4 @@ namespace rvcpp {
     }
     x.at(index) = value;
   }
-
-  Register& Registers::zero() {
-    x[0] = 0;  // manually enforce x0 is always zero
-    return x[0];
-  }
-  Register& Registers::ra() { return x[1]; }
-  Register& Registers::sp() { return x[2]; }
-  Register& Registers::gp() { return x[3]; }
-  Register& Registers::tp() { return x[4]; }
-  Register& Registers::t0() { return x[5]; }
-  Register& Registers::t1() { return x[6]; }
-  Register& Registers::t2() { return x[7]; }
-  Register& Registers::s0() { return x[8]; }
-  Register& Registers::fp() { return x[8]; }
-  Register& Registers::s1() { return x[9]; }
-  Register& Registers::a0() { return x[10]; }
-  Register& Registers::a1() { return x[11]; }
-  Register& Registers::a2() { return x[12]; }
-  Register& Registers::a3() { return x[13]; }
-  Register& Registers::a4() { return x[14]; }
-  Register& Registers::a5() { return x[15]; }
-  Register& Registers::a6() { return x[16]; }
-  Register& Registers::a7() { return x[17]; }
-  Register& Registers::s2() { return x[18]; }
-  Register& Registers::s3() { return x[19]; }
-  Register& Registers::s4() { return x[20]; }
-  Register& Registers::s5() { return x[21]; }
-  Register& Registers::s6() { return x[22]; }
-  Register& Registers::s7() { return x[23]; }
-  Register& Registers::s8() { return x[24]; }
-  Register& Registers::s9() { return x[25]; }
-  Register& Registers::s10() { return x[26]; }
-  Register& Registers::s11() { return x[27]; }
-  Register& Registers::t3() { return x[28]; }
-  Register& Registers::t4() { return x[29]; }
-  Register& Registers::t5() { return x[30]; }
-  Register& Registers::t6() { return x[31]; }
 }  // namespace rvcpp
